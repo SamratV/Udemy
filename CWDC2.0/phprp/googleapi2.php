ace538e643deb74d0589e43c9e26ce84c9acdfee
Vaibhaw Samrat <28707400+SamratV@users.noreply.github.com>
2021-06-29 17:54:55 +0530

    Update googleapi2.php

--- CWDC2.0/phprp/googleapi2.php
+++ CWDC2.0/phprp/googleapi2.php
@@ -7,7 +7,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script>
 			$.ajax({
-				url: "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBIe6ilKs8BD3oDKTDkywMiD6ObZFqL2d8",
+				url: "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY",
 				type: "GET",
 				success: function(data){
 					$.each(data["results"][0]["address_components"][6],function(key,value){
